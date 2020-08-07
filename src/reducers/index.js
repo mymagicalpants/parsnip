a2831b5be2bb2c800eb7ea237d1c7b8b6743c9e1
Scott Abrams <sabrams@trueimpact.com>
2020-08-07 10:41:32 -0400

    intial commit of redux exercises

---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+import { uniqueId } from "../actions";
+
+const mockTasks = [
+    {
+      id : uniqueId(),
+      title : 'Learn Redux',
+      description : 'The store, actions and reducers, oh my!',
+      status : 'In Progress'
+    },
+    {
+      id : uniqueId(),
+      title : 'Peace on Earth',
+      description : 'No big deal.',
+      status : 'In Progress'
+    }
+];
+
+export default function tasks(state = { tasks : mockTasks }, action) {
+    switch(action.type) {
+        case 'CREATE_TASK':
+            return { tasks : state.tasks.concat(action.payload) };
+        case 'EDIT_TASK':
+            const { payload } = action;
+              return {
+                tasks : state.tasks.map(task => {
+                  if (task.id === payload.id) {
+                    return Object.assign({}, task, payload.params);
+                  }
+
+                  return task;
+                })
+              }
+        default:
+            return state;
+    };
+}
